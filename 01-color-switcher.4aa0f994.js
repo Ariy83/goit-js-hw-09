e5f37b0bdb3b907cba089d7720fcb98942e6f4e1
Ariy83 <143614766+Ariy83@users.noreply.github.com>
2023-11-08 15:05:57 +0000

    Deploying to gh-pages from @ Ariy83/goit-js-hw-09@71950ce17c71d896428aadb1966bbd67f61fa71a 🚀

---
+++ 01-color-switcher.4aa0f994.js
@@ -0,0 +1,3 @@
+document.querySelector('button[data-start="Start"]');//# sourceMappingURL=01-color-switcher.4aa0f994.js.map
+
+//# sourceMappingURL=01-color-switcher.4aa0f994.js.map
---
+++ 01-color-switcher.8c920527.js
@@ -0,0 +1,3 @@
+document.querySelector('button[data-start="Start"]');//# sourceMappingURL=01-color-switcher.8c920527.js.map
+
+//# sourceMappingURL=01-color-switcher.8c920527.js.map
